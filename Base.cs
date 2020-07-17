22d1a09f40e7d494a780ff4f3d0e2264b797f033
fribeiro-wtf <54911892+fribeiro-wtf@users.noreply.github.com>
2020-07-17 14:11:07 -0300

    primeiro commit

---
+++ Base.cs
@@ -0,0 +1,79 @@
+﻿using java.io;
+using java.net;
+using org.apache.pdfbox.pdmodel;
+using org.apache.pdfbox.util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+
+namespace EasyAutomationFramework
+{
+    public static class Base
+    {
+        /// <summary>
+        /// Método para extrair texto de um PDF
+        /// </summary>
+        /// <param name="caminhoArquivo"></param>
+        /// <returns></returns>
+        public static string ExtractTextPdf(string caminhoArquivo)
+        {
+            try
+            {
+                URL url = new URL(caminhoArquivo);
+                InputStream ist = url.openStream();
+                BufferedInputStream fileToParse = new BufferedInputStream(ist);
+                PDDocument document = null;
+                String output = null;
+                try
+                {
+                    document = PDDocument.load(fileToParse);
+                    output = new PDFTextStripper().getText(document);
+                }
+                finally
+                {
+                    if (document != null)
+                    {
+                        document.close();
+                    }
+                    fileToParse.close();
+                    ist.close();
+                }
+                return output;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public static DataTable ConvertTo<T>(IList<T> list)
+        {
+            DataTable table = CreateTable<T>();
+            Type entityType = typeof(T);
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entityType);
+            foreach (T item in list)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                {
+                    row[prop.Name] = prop.GetValue(item);
+                }
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+        private static DataTable CreateTable<T>()
+        {
+            Type entityType = typeof(T);
+            DataTable table = new DataTable(entityType.Name);
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(entityType);
+            foreach (PropertyDescriptor prop in properties)
+            {
+                table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+            return table;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associada a um assembly.
+[assembly: AssemblyTitle("Easy.Automation")]
+[assembly: AssemblyDescription("Library to facilitate the construction of process automation.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Filipe Ribeiro Brito")]
+[assembly: AssemblyProduct("EasyAutomationFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("Filipe Ribeiro Brito")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("10273d2e-e190-492b-bdc9-bb3960cd4e57")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
